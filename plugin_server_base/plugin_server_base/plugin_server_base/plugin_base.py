1e7660e4ebe677f32d0a5f2ed4b2d1fed66f8cb2
Pedro Soares <pmbs.123@gmail.com>
2024-10-29 15:57:48 +0000

    feat: add typing and add try clock when calling tick

--- plugin_server_base/plugin_server_base/plugin_server_base/plugin_base.py
+++ plugin_server_base/plugin_server_base/plugin_server_base/plugin_base.py
@@ -1,4 +1,6 @@
+from sys import exec_prefix
 from rclpy.node import Node
+from rclpy.service import SrvTypeResponse, SrvTypeRequest
 from plugin_services.srv import PluginServer
 import rclpy
 from enum import Enum
@@ -11,8 +13,8 @@ class NodeState(Enum):
 
 
 class PluginBase(Node):
-    tick_rate = 30
-    standalone = False
+    tick_rate: int = 30
+    standalone: bool = False
 
     def __init__(self, node_name: str) -> None:
         """Constructor of the class PluginBase.
@@ -26,16 +28,14 @@ class PluginBase(Node):
         self._read_base_params()
 
         if self.standalone:
-            self.tick_timer = self.create_timer(
-                1 / self.tick_rate, self._tick_callback
-            )
+            self.tick_timer = self.create_timer(1 / self.tick_rate, self._tick_callback)
 
         # Action server object which will control when the plugin runs
         self._server = self.create_service(
             PluginServer, f"{node_name}/bt_server", self._service_callback
         )
 
-    def _read_base_params(self):
+    def _read_base_params(self) -> None:
         self.declare_parameter("tick_rate", self.tick_rate)
         self.declare_parameter("standalone", self.standalone)
 
@@ -48,18 +48,14 @@ class PluginBase(Node):
 
     def _tick_callback(self) -> None:
         """Callback method for the tick_timer."""
-        self.tick()
+        try:
+            self.tick()
+        except Exception as e:
+            self.get_logger().error(f"Tick Method has failed to run with {e}")
 
-    def tick(self) -> NodeState:
-        """This method should be implemented!
-        You should place here what you want your node to do.
-        It gets called 20 times a second if state=RUNNING
-        """
-        assert (
-            False
-        ), "Please make sure to implement the tick method of you Node"
-
-    def _service_callback(self, request, response):
+    def _service_callback(
+        self, request: PluginServer, response: PluginServer
+    ) -> PluginServer:
         """Sets the current state of the node based on the action request
         This will allow the node to be controlled by the BT_server
         This method should not be modified
@@ -69,19 +65,24 @@ class PluginBase(Node):
         # This way, if the node fails, the BT will now and start its fallback
         # procedure
 
-        self.get_logger().info(
-            f"Plugin <{self.plugin_name}> has been requested to be ticked."
+        self.get_logger().debug(
+            f"Plugin <{self.plugin_name}> has been requested to be ticked with {request}"
         )
 
         node_state = self.tick()
 
-        self.get_logger().info(
-            f"Plugin <{self.plugin_name}> state is: {node_state}"
-        )
+        self.get_logger().debug(f"Plugin <{self.plugin_name}> state is: {node_state}")
 
         response.node_state = node_state.value
 
         return response
+
+    def tick(self) -> NodeState:
+        """This method should be implemented!
+        You should place here what you want your node to do.
+        It gets called `tick_rate` times a second if state=RUNNING
+        """
+        assert False, "Please make sure to implement the tick method of you Node"
 
 
 def main():
