0eec37717252beeb34df459ec1040a226bce890d
Pedro Soares <pmbs.123@gmail.com>
2024-11-04 14:10:08 +0000

    feat: create a template to check if a plugin is currently selected

---
+++ tello_bt/tello_bt/actions/can_run_plugin.py
@@ -0,0 +1,27 @@
+import py_trees
+
+
+class CanRunPlugin(py_trees.behaviour.Behaviour):
+    """A helper class to check whether a plugin is seletec to run.
+
+    Uses the blackboard with name PluginsBlackboard, where it has
+    a key selected_plugin. If the value is the plugin_name, it returns
+    SUCCESS and FAILURE otherwise.
+    """
+
+    def __init__(self, name: str, plugin_name: str):
+        super().__init__(name)
+        self.plugin_name = plugin_name
+
+    def setup(self):  # type: ignore
+        self.blackboard = py_trees.blackboard.Client(name="PluginsBlackboard")
+        self.blackboard.register_key(
+            "selected_plugin", access=py_trees.common.Access.READ
+        )
+
+    def update(self):
+        selected_plugin = self.blackboard.selected_plugin
+
+        if selected_plugin == self.plugin_name:
+            return py_trees.common.Status.SUCCESS
+        return py_trees.common.Status.FAILURE
