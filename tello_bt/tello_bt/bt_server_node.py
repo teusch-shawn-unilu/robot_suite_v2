25079b639df6e652dd0e16961d23c6a87a67647f
Pedro Soares <pmbs.123@gmail.com>
2024-11-04 14:09:32 +0000

    feat: create a BT and run it continously

---
+++ tello_bt/tello_bt/bt_server_node.py
@@ -0,0 +1,43 @@
+import time
+import py_trees
+from py_trees.trees import BehaviourTree
+from rclpy.logging import rclpy
+from rclpy.node import Node
+from tello_bt.tree import Root
+
+
+class BtServerNode(Node):
+    bt_tick_freq: int = 60  # Hz
+
+    def __init__(self, node_name):
+        super().__init__(node_name)
+
+        self.bt = BehaviourTree(root=Root("Root", True, self))
+        self.bt.setup()
+
+    def read_params(self):
+        pass
+
+    def print_tree(self, tree: BehaviourTree):
+        print(py_trees.display.unicode_tree(root=tree.root, show_status=True))
+
+    def run(self):
+        while rclpy.ok():
+            self.bt.tick(post_tick_handler=self.print_tree)
+            rclpy.spin_once(self, timeout_sec=1)
+            time.sleep(1 / self.bt_tick_freq)
+
+        self.bt.shutdown()
+
+
+def main():
+    rclpy.init()
+    node = BtServerNode("bt_server")
+
+    node.run()
+
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
