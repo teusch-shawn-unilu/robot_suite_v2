4460f3d197c45987e13096c4f94dc49cd4c76098
Pedro Soares <pmbs.123@gmail.com>
2025-03-12 09:02:23 +0000

    feat(tello_bt): add a stop_on_failure flag

--- tello_bt/tello_bt/bt_server_node.py
+++ tello_bt/tello_bt/bt_server_node.py
@@ -9,6 +9,7 @@ from tello_bt.bootstrap import bootstrap_bt, BootstrapError
 class BtServerNode(Node):
     _bt_tick_freq: int = 60  # Hz
     _bt_name: str = "default_bt"
+    _stop_on_failure: bool = False
 
     def __init__(self, node_name):
         super().__init__(node_name)
@@ -29,10 +30,14 @@ class BtServerNode(Node):
     def read_params(self):
         self.declare_parameter("bt_name", self._bt_name)
         self.declare_parameter("tick_freq", self._bt_tick_freq)
+        self.declare_parameter("stop_on_failure", self._stop_on_failure)
 
         self._bt_name = self.get_parameter("bt_name").get_parameter_value().string_value
         self._bt_tick_freq = (
             self.get_parameter("tick_freq").get_parameter_value().integer_value
+        )
+        self._stop_on_failure = (
+            self.get_parameter("stop_on_failure").get_parameter_value().bool_value
         )
 
     def print_tree(self, tree: BehaviourTree):
@@ -41,11 +46,15 @@ class BtServerNode(Node):
     def run(self):
         while rclpy.ok():
             self.bt.tick(post_tick_handler=self.print_tree)
-            # if self.bt.root.status == py_trees.common.Status.FAILURE:
-            #     break
+            if (
+                self.bt.root.status == py_trees.common.Status.FAILURE
+                and self._stop_on_failure
+            ):
+                break
             rclpy.spin_once(self, timeout_sec=1)
             time.sleep(1 / self._bt_tick_freq)
 
+        self.get_logger().warning("BT root has returned FAILURE. Shutingdown")
         self.bt.shutdown()
 
 
