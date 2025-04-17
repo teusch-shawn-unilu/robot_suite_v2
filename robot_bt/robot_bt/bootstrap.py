a8e38c668f880bc2b795821c451ca2eddf1a51c6
Pedro Soares <pmbs.123@gmail.com>
2025-04-17 12:34:28 +0000

    feat(bt): improve bootstrapping to allow any module

--- robot_bt/robot_bt/bootstrap.py
+++ robot_bt/robot_bt/bootstrap.py
@@ -2,28 +2,31 @@ import importlib
 import py_trees
 from typing import Callable, Optional
 from rclpy.node import Node
+from rclpy.logging import get_logger
 
 
 class BootstrapError(Exception):
     """
     Custom exception for bootstrap-related errors.
-
-    Raised when invalid input is provided to the `bootstrap_bt` function, such as
-    a file name containing a `.py` extension.
     """
 
     pass
 
 
 def bootstrap_bt(
-    file_name: str,
-) -> Optional[Callable[[Node], py_trees.behaviour.Behaviour]]:
+    file_name: str, module_name: Optional[str] = None
+) -> Callable[[Node], py_trees.behaviour.Behaviour]:
     """
     Dynamically imports a module and retrieves the `bootstrap` function for a behavior tree.
 
     Args:
         file_name (str): Name of the file (without the `.py` extension) containing the
-                         `bootstrap` function within the `robot_bt.bt` package.
+                        `bootstrap`.
+        module_name (Optional[str]): Module/Package name from which it should try
+            to import the file_name. If set to None, it will search on `robot_bt.bt`.
+            In case your file is located under `pkg_name.sub_pkg.file.py`, you need to
+            include `pkg_name.sub_pkg` as your module_name
+
 
     Returns:
         Optional[Callable[[Node], py_trees.behaviour.Behaviour]]:
@@ -35,19 +38,31 @@ def bootstrap_bt(
         BootstrapError: If the file name contains a `.py` extension.
         ImportError: If the module does not contain a `bootstrap` function or cannot be imported.
     """
-    if file_name.find(".py") != -1:
-        raise BootstrapError("File name contains .py extension.")
+    logger = get_logger("robot_bt.bootstrap")
 
-    module_name = f"robot_bt.bt.{file_name}"
+    if file_name.find(".py") != -1:
+        logger.warning(f"File name `{file_name}` contains .py extension. Removing it.")
+        file_name.removesuffix(".py")
+
+    if module_name is None:
+        module_name = f"robot_bt.bt.{file_name}"
+    else:
+        module_name = f"{module_name}.file_name"
+
     try:
         module = importlib.import_module(module_name)
 
         if hasattr(module, "bootstrap"):
             return module.bootstrap
         else:
-            raise ImportError(
-                f"Module '{module_name}' does not have a 'bootstrap' function."
-            )
+            err_msg = f"Unable to find 'bootstrap' function on `{module_name}.py`."
+            logger.error(err_msg)
+            raise BootstrapError(err_msg)
     except ImportError as e:
-        print(f"Error importing module: {e}")
-        return None
+        err_msg = f"Error while importing module {module_name}: {e}"
+        logger.error(err_msg)
+        raise BootstrapError(err_msg)
+    except Exception as e:
+        err_msg = f"Unknown error occured while importing module {module_name}: {e}"
+        logger.error(err_msg)
+        raise BootstrapError(err_msg)
--- robot_bt/robot_bt/bt_server_node.py
+++ robot_bt/robot_bt/bt_server_node.py
@@ -1,15 +1,17 @@
 import time
 import py_trees
 from py_trees.trees import BehaviourTree
+from rclpy import Optional
 from rclpy.logging import rclpy
 from rclpy.node import Node
-from robot_bt.bootstrap import bootstrap_bt, BootstrapError
+from robot_bt.bootstrap import bootstrap_bt
 
 
 class BtServerNode(Node):
     _bt_tick_freq: int = 60  # Hz
     _bt_name: str = "default_bt"
     _stop_on_failure: bool = False
+    bt: Optional[BehaviourTree]
 
     def __init__(self, node_name):
         super().__init__(node_name)
@@ -17,20 +19,20 @@ class BtServerNode(Node):
         self.read_params()
 
         try:
+            self.get_logger().info(f"Trying to bootstrap {self._bt_name} BT")
             bootstrap_fn = bootstrap_bt(self._bt_name)
-        except (ImportError, BootstrapError):
-            raise Exception("Failed to bootstrap BT")
-
-        if bootstrap_fn is None:
-            raise Exception("Ensure that your bootstrap function returns a tree")
-
-        self.bt = BehaviourTree(root=bootstrap_fn(self))
-        self.bt.setup()
+            self.get_logger().info(f"{self._bt_name} has been bootstrapped")
+            self.bt = BehaviourTree(root=bootstrap_fn(self))
+            self.bt.setup()
+        except:
+            self.get_logger().error(f"Unable to bootstrap BT -> {self._bt_name}")
+            self.bt = None
 
     def read_params(self):
         self.declare_parameter("bt_name", self._bt_name)
         self.declare_parameter("tick_freq", self._bt_tick_freq)
         self.declare_parameter("stop_on_failure", self._stop_on_failure)
+        self.declare_parameter("plugins", [])
 
         self._bt_name = self.get_parameter("bt_name").get_parameter_value().string_value
         self._bt_tick_freq = (
@@ -45,6 +47,12 @@ class BtServerNode(Node):
 
     def run(self):
         while rclpy.ok():
+            if self.bt is None:
+                self.get_logger().warning(
+                    "Behaviour Tree was not successfully bootstrapped. No BT to run!"
+                )
+                time.sleep(2)
+                continue
             self.bt.tick(post_tick_handler=self.print_tree)
             if (
                 self.bt.root.status == py_trees.common.Status.FAILURE
@@ -54,8 +62,10 @@ class BtServerNode(Node):
             rclpy.spin_once(self, timeout_sec=1)
             time.sleep(1 / self._bt_tick_freq)
 
-        self.get_logger().warning("BT root has returned FAILURE. Shutingdown")
-        self.bt.shutdown()
+        self.get_logger().warning("BT root has returned FAILURE. Shutting down")
+
+        if self.bt:
+            self.bt.shutdown()
 
 
 def main():
